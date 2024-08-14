8192aafe056172c84e1a9c15599ae2d22787fffe
Panghy1106 <Panghy1106@163.com>
2024-08-14 11:04:42 +0000

    [UPDATE]: support Chinese cypher

--- prompt_generator.py
+++ prompt_generator.py
@@ -10,18 +10,14 @@ def test(config):
     with open(config.getInputQueryPath(), 'rb') as file:
         for i, line in enumerate(file, start=1):
             line=line.strip()
-            print(f"行 {i}: {line}")
+            print(f"行 {i}: {line.decode('utf-8')}")
             input_stream = CypherStream(line)
             lexer = LcypherLexer(input_stream)
             token_stream = CommonTokenStream(lexer)
             parser = LcypherParser(token_stream)
             tree = parser.oC_Cypher()
-            
-            # print(tree.toStringTree(recog=parser)) #AST
-            # ruleNames = parser.ruleNames
             visitor = TransVisitor(config)
             visitor.visit(tree)
-            visitor.printPrompt()
 
 if __name__ == '__main__':
     config_path='/root/work_repo/Awesome-Text2GQL/config.json'
