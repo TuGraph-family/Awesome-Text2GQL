6bbfc442ededbbb41eaef0846e12ad29e6ed3ae5
akiba93 <sidingw93@gmail.com>
2024-12-17 16:54:02 +0800

    support local LLM

--- scripts/gen_question_directly_llm.sh
+++ scripts/gen_question_directly_llm.sh
@@ -4,6 +4,7 @@ MODE=100
 INPUT_DIR_OR_FILE='./input_examples/query_only.txt'
 OUTPUT_DIR="./output"
 SUFFIX='_gen'
+MODEL_PATH='../Qwen__Qwen2.5-Coder-14B-Instruct'
 
 echo "----------------Running generalize_llm.py to GEN_PROMPT_DIRECTLY----------------"
-python3 generalize_llm.py "$MODE" "$INPUT_DIR_OR_FILE" "$OUTPUT_DIR" "$SUFFIX"
+python3 generalize_llm.py "$MODE" "$INPUT_DIR_OR_FILE" "$OUTPUT_DIR" "$SUFFIX" "$MODEL_PATH":
