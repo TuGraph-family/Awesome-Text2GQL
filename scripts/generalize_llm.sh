41a708b5c0e4be0b3b7473658daee943825bc75e
akiba93 <>
2024-12-17 15:22:59 +0800

    update local model call

--- scripts/generalize_llm.sh
+++ scripts/generalize_llm.sh
@@ -4,6 +4,7 @@ MODE=300
 INPUT_DIR_OR_FILE='./input_examples/corpus.txt'
 OUTPUT_DIR="./output"
 SUFFIX='_general'
+MODEL_PATH='../Qwen__Qwen2.5-Coder-14B-Instruct'
 
 echo "----------------Running generalize_llm.py to GENERALIZE with query----------------"
-python3 generalize_llm.py "$MODE" "$INPUT_DIR_OR_FILE" "$OUTPUT_DIR" "$SUFFIX"
+python3 generalize_llm.py "$MODE" "$INPUT_DIR_OR_FILE" "$OUTPUT_DIR" "$SUFFIX" "$MODEL_PATH"
